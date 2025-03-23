360d031f2c89fc89fba78919712031ece6bb097a
johnfrancistm <johnfrancistm@gmail.com>
2025-03-23 13:01:07 +0800

    initial commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,5 @@
 
 import { Layout } from '@/components/layout/Layout';
-import { Hero } from '@/components/home/Hero';
-import { FeaturedCategories } from '@/components/home/FeaturedCategories';
 import { FeaturedProducts } from '@/components/home/FeaturedProducts';
 import { ArtisanStory } from '@/components/home/ArtisanStory';
 import { Testimonials } from '@/components/home/Testimonials';
@@ -10,8 +8,6 @@ import { Newsletter } from '@/components/home/Newsletter';
 const Index = () => {
   return (
     <Layout>
-      <Hero />
-      <FeaturedCategories />
       <FeaturedProducts />
       <ArtisanStory />
       <Testimonials />
