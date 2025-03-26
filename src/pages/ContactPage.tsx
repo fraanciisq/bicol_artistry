dc32ad5d0ba03def4bfa96f27fab93b49d128157
Lance009 <88927806+Lance0567@users.noreply.github.com>
2025-03-26 03:38:02 +0000

    corrected coordinates

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -174,7 +174,7 @@ const ContactPage = () => {
           <h2 className="text-2xl font-bold mb-6 text-center">Find Us</h2>
           <div className="aspect-video rounded-lg overflow-hidden border border-gray-200">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1699985000000!5m2!1sen!2s"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.3246399519808!2d123.72886632756503!3d13.141910890071388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a103546adc58a7%3A0xbd421f39e869718a!2sVan%20Artistry!5e0!3m2!1sen!2sph!4v1742960064249!5m2!1sen!2sph"
               width="100%"
               height="100%"
               style={{ border: 0 }}
